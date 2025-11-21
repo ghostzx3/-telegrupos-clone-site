8af03e9c725f0935e497e6fd360b44ba8b510e3a
ghostzx3 <neto.xsilva666@gmail.com>
2025-11-21 16:10:02 -0300

    Personalização: cores, logo, preços e textos

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -29,7 +29,7 @@ export function Header({ onSearch, onLoginClick, onSubmitGroupClick }: HeaderPro
             <svg className="w-6 h-6 text-white" viewBox="0 0 24 24" fill="currentColor">
               <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z"/>
             </svg>
-            <h1 className="text-white font-bold text-xl">telegrupos.com.br</h1>
+            <h1 className="text-white font-bold text-xl">Grupostelegramx.com</h1>
           </div>
 
           {/* Search Bar */}
