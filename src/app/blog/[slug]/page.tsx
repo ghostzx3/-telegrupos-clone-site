5ce18b15fb584f8eb4ebb29d0ff4def67ed28892
ghostzx3 <neto.xsilva666@gmail.com>
2025-12-06 00:29:05 -0300

    Corrigir erro crítico - Adicionar import useEffect faltante em blog/[slug]/page.tsx

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
 import { useParams, useRouter } from "next/navigation";
 import { Header } from "@/components/Header";
